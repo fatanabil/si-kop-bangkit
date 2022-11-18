68398665a9af209c56feab6aafbe2cbc91c763bb
fatanabil <fatanabil63@gmail.com>
2022-11-18 22:08:30 +0700

    create types

---
+++ types/detail_ins.d.ts
@@ -0,0 +1,7 @@
+interface DetailIns {
+  _id: string;
+  nama_ins: string;
+  kode_ins: string;
+}
+
+export default DetailIns;
---
+++ types/memberType.d.ts
@@ -0,0 +1,11 @@
+import DetailIns from "./detail_ins";
+
+interface MemberType {
+  _id: string;
+  no_rek: string;
+  nama_anggota: string;
+  kode_ins: string;
+  detail_ins: DetailIns;
+}
+
+export default MemberType;
