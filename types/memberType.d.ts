0e7cabdf0b89f686430e542ad5486568859fcd07
fatanabil <fatanabil63@gmail.com>
2022-11-19 23:06:59 +0700

    refactor type

--- types/memberType.d.ts
+++ types/memberType.d.ts
@@ -1,11 +1,11 @@
-import DetailIns from "./detail_ins";
+import InstansiType from "./instansiType";
 
 interface MemberType {
   _id: string;
   no_rek: string;
   nama_anggota: string;
   kode_ins: string;
-  detail_ins: DetailIns;
+  detail_ins: InstansiType;
 }
 
 export default MemberType;
