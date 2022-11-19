c4ca8cd55c4c1a633efb780338a911e979a0e30e
fatanabil <fatanabil63@gmail.com>
2022-11-19 22:46:32 +0700

    add title meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,26 @@
 import { ReactNode } from "react";
 import Navbar from "./navbar";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
+  title: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, title }: LayoutProps) {
   return (
-    <div>
-      <header>
-        <Navbar />
-      </header>
-      <main className="min-h-screen bg-slate-800">
-        <div className="px-4 pt-24 pb-16 sm:px-8 md:px-16">{children}</div>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div>
+        <header>
+          <Navbar />
+        </header>
+        <main className="min-h-screen bg-slate-800">
+          <div className="px-4 pt-24 pb-16 sm:px-8 md:px-16">{children}</div>
+        </main>
+      </div>
+    </>
   );
 }
