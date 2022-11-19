3b9d5c1d57d51d44b1798c2bb5d5ff80b857a1c0
fatanabil <fatanabil63@gmail.com>
2022-11-19 22:57:29 +0700

    add hover effect

--- components/buttons/searchButton.tsx
+++ components/buttons/searchButton.tsx
@@ -10,11 +10,11 @@ interface SearchButtonProps
 export default function SearchButton(props: SearchButtonProps) {
   return (
     <button
-      className="px-4 py-3 rounded-md text-white bg-teal-600 flex items-center gap-3 font-semibold"
+      className="px-4 py-3 rounded-md text-white bg-teal-600 flex items-center gap-3 font-semibold hover:bg-teal-700 transition-all"
       {...props}
     >
       {props.loading ? <Loader /> : <FaSearch />}
-      <span>Cari</span>
+      <span>Cari Data</span>
     </button>
   );
 }
