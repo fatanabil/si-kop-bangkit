47a8fd2673a3b3ee0fbc9fdf7b4469e447409540
fatanabil <fatanabil63@gmail.com>
2022-11-20 22:49:11 +0700

    migrate to one file

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+export interface MemberType {
+  _id: string;
+  no_rek: string;
+  nama_anggota: string;
+  kode_ins: string;
+  detail_ins: AgencyType;
+}
+
+export interface AgencyType {
+  _id: string;
+  nama_ins: string;
+  kode_ins: string;
+}
+
+export interface AuthDataType {
+  username: string;
+  token: string;
+  isAuthenticated: boolean;
+}
--- types/instansiType.ts
+++
@@ -1,7 +0,0 @@
-interface InstansiType {
-  _id: string;
-  nama_ins: string;
-  kode_ins: string;
-}
-
-export default InstansiType;
