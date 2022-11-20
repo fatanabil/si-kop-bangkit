c4d9baa7ec1b669c4f30b10d5f2b79149dc971d7
fatanabil <fatanabil63@gmail.com>
2022-11-20 22:50:02 +0700

    setup auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AuthContextProvider } from "../contexts/authContext";
+import { useState, useMemo } from "react";
+import { AuthDataType } from "../types";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [authData, setAuthData] = useState({
+    username: "",
+    token: "",
+    isAuthenticated: false,
+  });
+
+  const changeAuthData = (newVal: AuthDataType) => {
+    setAuthData((prevVal) => {
+      return {
+        ...prevVal,
+        ...newVal,
+      };
+    });
+  };
+
+  const authDataContextValue = useMemo(() => {
+    return {
+      authData,
+      changeAuthData,
+    };
+  }, [authData]);
+
+  return (
+    <AuthContextProvider value={authDataContextValue}>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
