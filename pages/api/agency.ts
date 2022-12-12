c2448db0970b2232beceec4fa3daca5b4a32bf65
fatanabil <fatanabil63@gmail.com>
2022-12-12 14:14:26 +0700

    add token verify

--- pages/api/agency.ts
+++ pages/api/agency.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connect } from "../../middlewares/mongodb";
+import verifyToken from "../../middlewares/tokenVerify";
 import Instansi from "../../models/agencyModels";
 
 export default async function handler(
@@ -11,8 +12,26 @@ export default async function handler(
   const {
     query: { nama_ins },
     method,
+    headers,
   } = req;
 
+  const token = headers.authorization as string;
+  let refreshToken: string = "";
+
+  const result = verifyToken({ token });
+
+  if (result?.data?.refreshToken) {
+    refreshToken = result.data.refreshToken;
+  }
+
+  if (result?.err) {
+    if (result.msg === "Unauthorized") {
+      return res.status(401).json({ data: [], msg: "Unauthorized", err: true });
+    }
+    return res.status(500).json({ data: [], msg: "Token expired", err: true });
+  }
+
+  // Request handler
   if (method === "GET") {
     let agencyData;
 
@@ -30,6 +49,15 @@ export default async function handler(
     }
 
     agencyData = await Instansi.find();
+
+    if (refreshToken) {
+      return res.status(200).json({
+        data: agencyData,
+        msg: "Success get data",
+        error: false,
+        refreshToken,
+      });
+    }
 
     return res
       .status(200)
