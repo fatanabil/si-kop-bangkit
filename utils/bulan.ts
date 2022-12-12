9d4822839eaa5d33c79de9b8abe4d0378e7c8991
fatanabil <fatanabil63@gmail.com>
2022-12-12 20:31:28 +0700

    create months util

---
+++ utils/bulan.ts
@@ -0,0 +1,16 @@
+const months = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "November",
+  "Desember",
+];
+
+export default months;
