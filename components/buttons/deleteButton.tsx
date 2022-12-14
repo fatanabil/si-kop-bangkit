f5a76c25ba4c72c445ed5386bdbb98ce920aad74
fatanabil <fatanabil63@gmail.com>
2022-12-15 01:04:22 +0700

    fix delete button

--- components/buttons/deleteButton.tsx
+++ components/buttons/deleteButton.tsx
@@ -1,12 +1,19 @@
+import { ReactNode } from "react";
 import { FaTrash } from "react-icons/fa";
 
 interface DeleteButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string | ReactNode;
+}
 
 export default function DeleteButton(props: DeleteButtonProps) {
   return (
-    <button {...props}>
-      <FaTrash className="text-red-500 hover:text-red-400" />
+    <button
+      {...props}
+      className="px-4 py-2 gap-3 bg-rose-500 text-white rounded-md flex items-center hover:bg-rose-400 transition-all"
+    >
+      <FaTrash className="text-white" />
+      {props.children}
     </button>
   );
 }
