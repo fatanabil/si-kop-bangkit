689c98f176e0b16164c62082cffcd1ba7a405884
fatanabil <fatanabil63@gmail.com>
2022-12-16 13:29:15 +0700

    change mongodb to production

--- middlewares/mongodb.ts
+++ middlewares/mongodb.ts
@@ -3,9 +3,7 @@ import mongoose, { Mongoose } from "mongoose";
 const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 export const connect = async () => {
-  //   const mongodburi = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.yesqe.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-  const mongodburi =
-    "mongodb://localhost:27017/kop-bangkit?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  const mongodburi = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.yesqe.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
   const conn = await mongoose
     .connect(mongodburi as string)
