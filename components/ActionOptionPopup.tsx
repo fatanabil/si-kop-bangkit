ec40b6cd193e829e2856be7f191fa5824d83c4c0
fatanabil <fatanabil63@gmail.com>
2024-11-14 13:29:18 +0700

    fix: outside click handler making button not work

--- components/ActionOptionPopup.tsx
+++ components/ActionOptionPopup.tsx
@@ -1,4 +1,3 @@
-import { Ref, useEffect, useRef } from 'react';
 import PencilIcon from '../icons/PencilIcon';
 import TrashIcon from '../icons/TrashIcon';
 import { MemberType } from '../types';
@@ -12,39 +11,30 @@ interface ActionOptionPopupType {
 }
 
 const ActionOptionPopup = ({ openOption, setOpenOption, member, handleOnEdit, handleOnDelete }: ActionOptionPopupType) => {
-    const ref = useRef<HTMLDivElement | null>(null);
-
-    const handleClickOutside = (ev: MouseEvent) => {
-        if (ref.current && !ref.current.contains(ev.target as Node)) {
-            setOpenOption({ status: false, id: '' });
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-
-        return () => document.removeEventListener('mousedown', handleClickOutside);
-    }, []);
-
     return (
-        <div
-            ref={ref}
-            className={`flex flex-col bg-slate-500 rounded-md absolute z-10 right-0 transition-all duration-150 origin-top-right divide-y-[1px] divide-slate-400 overflow-hidden shadow-lg ${
-                openOption.status && openOption.id === member._id ? 'opacity-100 scale-100' : 'opacity-0 -translate-y-4 scale-90 pointer-events-none'
-            }`}
-        >
-            <button onClick={() => handleOnEdit(member)} className='flex gap-2 items-center cursor-pointer px-5 py-[6px] hover:bg-slate-600 transition-all duration-150'>
-                <PencilIcon className='w-4 h-4' />
-                <span>Edit</span>
-            </button>
-            <button
-                onClick={() => handleOnDelete(member)}
-                className='flex gap-2 items-center cursor-pointer px-5 py-[6px] hover:bg-slate-600 transition-all duration-150 text-rose-500'
+        <>
+            {openOption.status && <div onClick={() => setOpenOption({ status: false, id: '' })} className='w-full h-full bg-transparent fixed top-0 left-0 z-0'></div>}
+            <div
+                className={`flex flex-col bg-slate-500 rounded-md absolute z-10 right-0 transition-all duration-150 origin-top-right divide-y-[1px] divide-slate-400 overflow-hidden shadow-lg ${
+                    openOption.status && openOption.id === member._id ? 'opacity-100 scale-100' : 'opacity-0 -translate-y-4 scale-90 pointer-events-none'
+                }`}
             >
-                <TrashIcon className='w-4 h-4' />
-                <span>Delete</span>
-            </button>
-        </div>
+                <button
+                    onClick={() => handleOnEdit(member)}
+                    className='flex gap-2 items-center cursor-pointer px-5 py-[6px] hover:bg-slate-600 transition-all duration-150'
+                >
+                    <PencilIcon className='w-4 h-4' />
+                    <span>Edit</span>
+                </button>
+                <button
+                    onClick={() => handleOnDelete(member)}
+                    className='flex gap-2 items-center cursor-pointer px-5 py-[6px] hover:bg-slate-600 transition-all duration-150 text-rose-500'
+                >
+                    <TrashIcon className='w-4 h-4' />
+                    <span>Delete</span>
+                </button>
+            </div>
+        </>
     );
 };
 
